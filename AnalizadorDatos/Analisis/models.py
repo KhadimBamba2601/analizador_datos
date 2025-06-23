47264e767e54c5425d1a0dc1a34ba8ef5070081a
KhadimBamba2601 <trabajoempresakn@gmail.com>
2025-06-23 14:59:17 +0200

    corrreccion .gitignore

--- AnalizadorDatos/Analisis/models.py
+++ AnalizadorDatos/Analisis/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from encrypted_fields import fields
 
 class ConjuntoDatos(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     archivo = models.FileField(upload_to='datasets/')
     subido_en = models.DateTimeField(auto_now_add=True)
-    nombre = models.CharField(max_length=255)
+    nombre = fields.EncryptedCharField(max_length=255)
 
 class ResultadoAnalisis(models.Model):
     conjunto_datos = models.ForeignKey(ConjuntoDatos, on_delete=models.CASCADE)
-    estadisticas = models.JSONField()  # Almacena media, mediana, correlación
+    estadisticas = models.JSONField()
     grafico = models.ImageField(upload_to='plots/', null=True)
